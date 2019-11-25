320324626b11deeea0456d62b8ef70ad86fabd82
Emerson Spradling <espradli@trinity.edu>
2019-11-25 15:24:31 -0600

    attempting randomsplit

--- src/main/scala/chicagotransportation/ChicagoTransportation.scala
+++ src/main/scala/chicagotransportation/ChicagoTransportation.scala
@@ -1,11 +1,7 @@
 package chicagotransportation
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.ml.feature.VectorAssembler
-import org.apache.spark.ml.feature.StandardScaler
-import org.apache.spark.ml.clustering.KMeans
 import swiftvis2.plotting.Plot
 import swiftvis2.plotting._
 import swiftvis2.plotting.renderer.SwingRenderer
@@ -15,9 +11,13 @@ object ChicagoTransportation{
   def main(args: Array[String]): Unit ={
     val spark = SparkSession.builder().master("local[*]").appName("SparkSQL2").getOrCreate()
     import spark.implicits._
-  
+    
     spark.sparkContext.setLogLevel("WARN")
     
-    
+    lazy val taxiData = spark.read.option("inferSchema", true).
+      option("header", "true").
+      csv("/data/BigData/students/espradli/taxiData.csv")
+
+    val randomSample = taxiData.randomSplit(Array(.05, .995))(0).write.csv("/data/BigData/students/espradli/taxiDataRandom.csv")
   }
 }
